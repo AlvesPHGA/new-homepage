c0923912ff3f62e16c18bc30ffce713226cdf420
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-12-31 16:04:42 -0300

    was a clean code on nav in header

--- src/components/global/Header/Header.tsx
+++ src/components/global/Header/Header.tsx
@@ -1,41 +1,22 @@
 import Logo from '../../../assets/icon/logo.svg';
 
+const navLinksLegend = ['Home', 'New', 'Popular', 'Trending', 'Categories'];
+
 export function Header() {
    return (
       <header className="px-8">
          <div className="w-[75rem] py-10 mx-auto flex items-center justify-between font-inter">
             <img src={Logo} alt="Logo site" />
             <nav className="text-2xl">
-               <a
-                  className="hover:text-softRed transition-all delay-100 mr-6"
-                  href=""
-               >
-                  Home
-               </a>
-               <a
-                  className="hover:text-softRed transition-all delay-100 mr-6"
-                  href=""
-               >
-                  New
-               </a>
-               <a
-                  className="hover:text-softRed transition-all delay-100 mr-6"
-                  href=""
-               >
-                  Popular
-               </a>
-               <a
-                  className="hover:text-softRed transition-all delay-100 mr-6"
-                  href=""
-               >
-                  Trending
-               </a>
-               <a
-                  className="hover:text-softRed transition-all delay-100"
-                  href=""
-               >
-                  Categories
-               </a>
+               {navLinksLegend.map((legend) => (
+                  <a
+                     key={legend}
+                     className="hover:text-softRed transition-all delay-100 mr-6 last:mr-0"
+                     href=""
+                  >
+                     {legend}
+                  </a>
+               ))}
             </nav>
          </div>
       </header>
