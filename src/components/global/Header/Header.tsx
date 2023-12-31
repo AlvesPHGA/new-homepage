ba36bf58a5c92ef398c0bf78bf8d417e663d8f11
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-12-31 16:29:47 -0300

    was started to edit responsive design

--- src/components/global/Header/Header.tsx
+++ src/components/global/Header/Header.tsx
@@ -4,14 +4,14 @@ const navLinksLegend = ['Home', 'New', 'Popular', 'Trending', 'Categories'];
 
 export function Header() {
    return (
-      <header className="px-8">
-         <div className="w-[75rem] py-10 mx-auto flex items-center justify-between font-inter">
+      <header className="xl:2xl:px-8">
+         <div className="xl:2xl:w-[75rem] xl:2xl:py-10 xl:2xl:mx-auto xl:2xl:flex xl:2xl:items-center xl:2xl:justify-between xl:2xl:font-inter">
             <img src={Logo} alt="Logo site" />
-            <nav className="text-2xl">
+            <nav className="xl:2xl:text-2xl">
                {navLinksLegend.map((legend) => (
                   <a
                      key={legend}
-                     className="hover:text-softRed transition-all delay-100 mr-6 last:mr-0"
+                     className="xl:2xl:hover:text-softRed xl:2xl:transition-all xl:2xl:delay-100 xl:2xl:mr-6 xl:2xl:last:mr-0"
                      href=""
                   >
                      {legend}
