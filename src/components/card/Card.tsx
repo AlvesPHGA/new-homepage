156b8e48b4d5c8f9db011dcc7a717eac1a4931ce
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-12-31 12:20:34 -0300

    was added hover action in cards

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -3,19 +3,19 @@ import image01 from '../../assets/images/image-retro-pcs.jpg';
 
 export function Card() {
    return (
-      <div className="flex gap-6 w-wCard h-fit font-inter">
+      <a className="flex gap-6 w-wCard h-fit font-inter group cursor-pointer">
          <div>
             <img src={image01} alt="" className="object-fill" />
          </div>
          <div className="flex flex-col justify-center gap-3 ">
             <span className="text-4xl font-bold text-grayishBlue">01</span>
-            <h2 className="text-xl text-veryDarkBlue font-extra-bold">
+            <h2 className="text-xl text-veryDarkBlue font-extra-bold group-hover:text-softRed transition-all delay-200">
                Reviving Retro PCs
             </h2>
             <p className="text-sm pr-6 text-darkGrayishBlue">
                What happens when old PCs are given modern upgrades?
             </p>
          </div>
-      </div>
+      </a>
    );
 }
