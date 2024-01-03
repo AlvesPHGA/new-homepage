412759e03291c9bb0adfb33a1ea4d15115169a18
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-01-02 22:54:21 -0300

    updated layout to tablet device in aside

--- src/components/aside/Aside.tsx
+++ src/components/aside/Aside.tsx
@@ -1,12 +1,12 @@
 export function AsideContent() {
    return (
-      <aside className="bg-veryDarkBlue font-inter xl:py-5 xl:px-5 2xl:py-5 2xl:px-5 lg:py-4 lg:px-5">
-         <h2 className="text-softOrange font-bold xl:text-5xl 2xl:text-5xl lg:text-4xl">
+      <aside className="bg-veryDarkBlue font-inter xl:py-5 xl:px-5 2xl:py-5 2xl:px-5 lg:py-4 lg:px-5 md:px-2 md:py-2">
+         <h2 className="text-softOrange font-bold xl:text-5xl 2xl:text-5xl lg:text-4xl md:text-2xl">
             New
          </h2>
 
-         <a className="block cursor-pointer group xl:mt-8 xl:mb-8 xl:pr-6 2xl:mt-8 2xl:mb-8 2xl:pr-6 lg:mt-2 lg:mb-4 lg:pr-6">
-            <h3 className="text-white text-lg font-bold xl:group-hover:text-softOrange xl:transition-all xl:delay-200 2xl:group-hover:text-softOrange 2xl:transition-all 2xl:delay-200 lg:group-hover:text-softOrange lg:transition-all lg:delay-200">
+         <a className="block cursor-pointer group xl:mt-8 xl:mb-8 xl:pr-6 2xl:mt-8 2xl:mb-8 2xl:pr-6 lg:mt-2 lg:mb-4 lg:pr-6 md:mt-1 md:mb-4">
+            <h3 className="text-white text-lg font-bold xl:group-hover:text-softOrange xl:transition-all xl:delay-200 2xl:group-hover:text-softOrange 2xl:transition-all 2xl:delay-200 lg:group-hover:text-softOrange lg:transition-all lg:delay-200 md:text-base">
                Hydrogen VS Electric Cars
             </h3>
             <p className="text-grayishBlue xl:leading-7 2xl:leading-7 lg:leading-7">
@@ -14,8 +14,8 @@ export function AsideContent() {
             </p>
          </a>
 
-         <a className="border-t border-b border-darkGrayishBlue block cursor-pointer group xl:py-8 xl:pr-4 2xl:py-8 2xl:pr-4 lg:py-6 lg:pr-4">
-            <h3 className="text-white text-lg font-bold xl:group-hover:text-softOrange xl:transition-all xl:delay-200 2xl:group-hover:text-softOrange 2xl:transition-all 2xl:delay-200 lg:group-hover:text-softOrange lg:transition-all lg:delay-200">
+         <a className="border-t border-b border-darkGrayishBlue block cursor-pointer group xl:py-8 xl:pr-4 2xl:py-8 2xl:pr-4 lg:py-6 lg:pr-4 md:py-4 md:pr-0">
+            <h3 className="text-white text-lg font-bold xl:group-hover:text-softOrange xl:transition-all xl:delay-200 2xl:group-hover:text-softOrange 2xl:transition-all 2xl:delay-200 lg:group-hover:text-softOrange lg:transition-all lg:delay-200 md:text-base">
                The Downsides of Al Artistry
             </h3>
             <p className="text-grayishBlue xl:leading-7 2xl:leading-7 lg:leading-7">
@@ -25,7 +25,7 @@ export function AsideContent() {
          </a>
 
          <a className="block cursor-pointer group">
-            <h3 className="text-white text-lg font-bold xl:mt-8 xl:group-hover:text-softOrange xl:transition-all xl:delay-200 2xl:mt-8 2xl:group-hover:text-softOrange 2xl:transition-all 2xl:delay-200 lg:mt-4 lg:group-hover:text-softOrange lg:transition-all lg:delay-200">
+            <h3 className="text-white text-lg font-bold xl:mt-8 xl:group-hover:text-softOrange xl:transition-all xl:delay-200 2xl:mt-8 2xl:group-hover:text-softOrange 2xl:transition-all 2xl:delay-200 lg:mt-4 lg:group-hover:text-softOrange lg:transition-all lg:delay-200 md:text-base md:mt-4">
                Is VC Funding Drying Up?
             </h3>
             <p className="text-grayishBlue leading-7">
