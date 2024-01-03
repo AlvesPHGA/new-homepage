c0e18c7e397298baf91b2b7b06cc49819f43ea8c
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-01-02 22:16:00 -0300

    updated header layout to tablet

--- src/components/global/Header/Header.tsx
+++ src/components/global/Header/Header.tsx
@@ -5,9 +5,9 @@ const navLinksLegend = ['Home', 'New', 'Popular', 'Trending', 'Categories'];
 export function Header() {
    return (
       <header className="xl:2xl:px-8">
-         <div className="font-inter xl:2xl:w-[75rem] xl:2xl:py-10 xl:2xl:lg:mx-auto xl:2xl:lg:flex xl:2xl:items-center xl:2xl:justify-between lg:w-[1000px] lg:mx-auto lg:flex lg:items-center lg:justify-between lg:py-5 border">
+         <div className="font-inter xl:2xl:w-[75rem] xl:2xl:py-10 xl:2xl:lg:mx-auto xl:2xl:lg:flex xl:2xl:items-center xl:2xl:justify-between lg:w-[1000px] lg:mx-auto lg:flex lg:items-center lg:justify-between lg:py-5 md:w-[calc(768px-40px)] md:flex md:items-center md:mx-auto md:py-6 border">
             <img src={Logo} alt="Logo site" />
-            <nav className="xl:2xl:text-2xl lg:text-xl">
+            <nav className="xl:2xl:text-2xl lg:text-xl md:hidden">
                {navLinksLegend.map((legend) => (
                   <a
                      key={legend}
