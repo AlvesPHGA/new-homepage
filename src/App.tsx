1ae3b40698b0fa2014f75fa5229b4f1b4a3b0b5f
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-01-03 22:25:57 -0300

    updated main content layout to mobile s

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ function App() {
       <>
          <Header />
 
-         <main className="xl:w-[75rem] 2xl:w-[75rem] lg:w-[1000px] mbl_s:w-full xl:mx-auto 2xl:mx-auto lg:mx-auto md:w-[calc(768px-40px)] md:mx-auto">
-            <section className="xl:w-full xl:grid xl:grid-cols-main xl:justify-between  2xl:w-full 2xl:grid 2xl:grid-cols-main 2xl:justify-between lg:w-full lg:grid lg:grid-cols-main lg:justify-between md:grid md:w-full md:grid-cols-main md:justify-between">
+         <main className="xl:w-[75rem] 2xl:w-[75rem] lg:w-[1000px] mbl_s:w-full xl:mx-auto 2xl:mx-auto lg:mx-auto md:w-[calc(768px-40px)] md:mx-auto mbl_s:px-4">
+            <section className="xl:w-full xl:grid xl:grid-cols-main xl:justify-between  2xl:w-full 2xl:grid 2xl:grid-cols-main 2xl:justify-between lg:w-full lg:grid lg:grid-cols-main lg:justify-between md:grid md:w-full md:grid-cols-main md:justify-between mbl_s:w-full">
                <MainContent />
                <AsideContent />
             </section>
