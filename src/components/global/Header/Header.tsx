950c0d27d92837275dc6f0a0d2271fab375a51b2
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-01-07 10:54:14 -0300

    header config on css ok

--- src/components/global/Header/Header.tsx
+++ src/components/global/Header/Header.tsx
@@ -5,15 +5,11 @@ const navLinksLegend = ['Home', 'New', 'Popular', 'Trending', 'Categories'];
 export function Header() {
    return (
       <header className="header">
-         <div className="header__container xl:2xl:w-[75rem] lg:w-[1000px] md:w-[calc(768px-40px)]">
+         <div className="header__container">
             <img src={Logo} alt="Logo site" />
             <nav className="header__nav">
                {navLinksLegend.map((legend) => (
-                  <a
-                     key={legend}
-                     className="lg:hover:text-softRed lg:transition-all lg:delay-100 lg:mr-6 lg:last:mr-0"
-                     href=""
-                  >
+                  <a key={legend} href="">
                      {legend}
                   </a>
                ))}
